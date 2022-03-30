7f2cb9f1fa19c54bb4d64b612e3e88070e8bfff3
Sidel-Meril <pravoe.slovo.svydrygailo@gmail.com>
2022-03-30 05:19:27 +0300

    adding conversations

--- bot-test.py
+++ bot-test.py
@@ -237,8 +237,7 @@ def answer_response(update, context):
     answer = update.message.text
     updater.bot.send_message(chat_id=variables['telegram']['admin_id'], text="<b>Ответ администратора на Ваше сообщение</b>\n"+answer,
                              parse_mode='HTML')
-    updater.bot.send_message(chat_id=variables['telegram']['admin_id'], text="""Вы ответили пользователю <a href="tg://user?id=%s">id%s</a>""" %(user_id_to_response,
-                                                                                                                                                 user_id_to_response),
+    updater.bot.send_message(chat_id=variables['telegram']['admin_id'], text=f"""Вы ответили пользователю <a href="tg://user?id={user_id_to_response}">id{user_id_to_response}</a>""",
                              parse_mode='HTML')
     user_id_to_response = None
     return ConversationHandler.END
@@ -250,11 +249,11 @@ def accept(user_id):
     db.edit_user_by_id(user_id, 'paid', int(datetime.now().timestamp()), int(datetime.now().timestamp())+_duration)
     updater.bot.send_message(user_id, "Оператор рассмотрел вашу заявку, оплата принята")
     updater.bot.send_message(variables['telegram']['admin_id'],
-                             """Запрос принят. Уведомление успешно доставлено пользователю <a href="tg://user?id=%i">id%i</a>""" % (user_id, user_id ), parse_mode='HTML')
+                             f"""Запрос принят. Уведомление успешно доставлено пользователю <a href="tg://user?id={user_id}">id{user_id}</a>""" % (user_id, user_id ), parse_mode='HTML')
 
 def decline(user_id):
     updater.bot.send_message(user_id, "Оператор рассмотрел вашу заявку, что-то пошло не так :( \nОтправьте вашу заявку еще раз.")
-    updater.bot.send_message(variables['telegram']['admin_id'], """Запрос отклонен. Уведомление успешно доставлено пользователю <a href="tg://user?id=%i">id%i</a>""" %(user_id, user_id),
+    updater.bot.send_message(variables['telegram']['admin_id'], f"""Запрос отклонен. Уведомление успешно доставлено пользователю <a href="tg://user?id={user_id}">id{user_id}</a>""",
                              parse_mode = 'HTML')
 
 @admin
