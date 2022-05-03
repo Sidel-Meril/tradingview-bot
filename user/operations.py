7e7c043a75eeaf539d56d2ac4cf84dbc0c79301b
Sidel-Meril <pravoe.slovo.svydrygailo@gmail.com>
2022-05-03 15:29:09 +0300

    structuring project

--- user/operations.py
+++ user/operations.py
@@ -158,7 +158,7 @@ class User:
         try:
             pair, timeframe = update.message.text.split(' ')
             _text = self.Text.REQUEST_RECEIVED.format(data=f"%s %s" %(pair, timeframe))
-            self.msg.send_message(str(user_id), _text)
+            self.updater.bot.send_message(user_id, _text, parse_mode='HTML')
             if timeframe not in AdminText.RANGES:
                 self.msg.send_message(user_id, AdminText.TIMEFRAME)
                 return self.conversations['END']
